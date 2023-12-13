2a010f64633876be4a7862bc8f0b6646ffe8d69c
gitName <a@b.c>
2023-12-13 15:45:28 +0800

    remove secret

--- RedisStore.cs
+++ RedisStore.cs
@@ -2,7 +2,7 @@ using StackExchange.Redis;
 public class RedisStore
     {
         //static string connectionString = "10.1.0.7:6379,password=new.mypass,ssl=false,abortConnect=False";
-        static string connectionString = "myredis068.redis.cache.windows.net:6380,password=KFDagozAzZw4lZlC85a0iNeebPwmRQs2iAzCaGPsMLk=,ssl=True,abortConnect=False";
+        static string connectionString = "xxxx,ssl=True,abortConnect=False";
         private static readonly Lazy<ConnectionMultiplexer> LazyConnection;
 
         static RedisStore()
